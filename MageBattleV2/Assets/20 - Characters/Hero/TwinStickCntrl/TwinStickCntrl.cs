65bc5025efd79bbd5c63dd83ebbc1c85451cf2d9
upallnightcoding <upallnightcoding@gmail.com>
2023-10-20 00:04:43 -0400

    Development
    - Implemented Top/Down shooter control
    - Implemented Animation

--- MageBattleV2/Assets/20 - Characters/Hero/TwinStickCntrl/TwinStickCntrl.cs
+++ MageBattleV2/Assets/20 - Characters/Hero/TwinStickCntrl/TwinStickCntrl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,28 +6,35 @@ using UnityEngine.InputSystem;
 
 public class TwinStickCntrl : MonoBehaviour
 {
+    private const string ANIMATION_TURN = "TURN";
+    private const string ANIMATION_MOVE = "MOVE";
+
     [SerializeField] private float playerSpeed;
 
     private Vector2 playerMove;
-    private Vector2 playerLook;
-    private Vector3 direction;
+    private Vector2 playerAim;
+    private Vector3 moveDirection;
     private Vector3 rotationTarget;
+    private Vector3 aimDirection;
     private CharacterController charCntrl;
+    private Animator anim;
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        playerMove = context.ReadValue<Vector2>();
-    }
+    Transform cam;
+    Vector3 camForward;
+    Vector3 move;
+    Vector3 moveInput;
 
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        playerLook = context.ReadValue<Vector2>();
-    }
+    float forwardAmount;
+    float turnAmount;
+   
 
     // Start is called before the first frame update
     void Start()
     {
         charCntrl = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
@@ -34,15 +42,19 @@ public class TwinStickCntrl : MonoBehaviour
     {
         MovePlayer();
         AimPlayer();
+        AnimatePlayer();
+    }
+
+    private void AnimatePlayer()
+    {
+
     }
 
     private void AimPlayer()
     {
-        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(playerAim);
 
-        Ray ray = Camera.main.ScreenPointToRay(playerLook);
-
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             rotationTarget = hit.point;
         }
@@ -51,7 +63,9 @@ public class TwinStickCntrl : MonoBehaviour
         aim.y = 0.0f;
         Quaternion rotation = Quaternion.LookRotation(aim);
 
-        Vector3 aimDirection = new Vector3(rotationTarget.x, 0.0f, rotationTarget.z);
+        aimDirection.x = rotationTarget.x; ;
+        aimDirection.y = 0.0f;
+        aimDirection.z = rotationTarget.z;
 
         if (aimDirection != Vector3.zero)
         {
@@ -61,13 +75,35 @@ public class TwinStickCntrl : MonoBehaviour
 
     private void MovePlayer()
     {
-        direction.x = playerMove.x;
-        direction.y = 0.0f;
-        direction.z = playerMove.y;
+        moveDirection.x = playerMove.x; // Horizontal
+        moveDirection.y = 0.0f;
+        moveDirection.z = playerMove.y; // Vertical
 
-        if (direction != Vector3.zero)
+        camForward = Vector3.Scale(cam.up, new Vector3(1, 0, 1)).normalized;
+        move = playerMove.y * camForward + playerMove.x * cam.right;
+        move.Normalize();
+        moveInput = move;
+
+        Vector3 localMove = transform.InverseTransformDirection(moveInput);
+        turnAmount = localMove.x;
+        forwardAmount = localMove.z;
+
+        if (moveDirection != Vector3.zero)
         {
-            charCntrl.Move(direction * playerSpeed * Time.deltaTime);
+            charCntrl.Move(moveDirection * playerSpeed * Time.deltaTime);
+
+            anim.SetFloat(ANIMATION_TURN, turnAmount, 0.1f, Time.deltaTime);
+            anim.SetFloat(ANIMATION_MOVE, forwardAmount, 0.1f, Time.deltaTime);
         }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        playerMove = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        playerAim = context.ReadValue<Vector2>();
     }
 }
